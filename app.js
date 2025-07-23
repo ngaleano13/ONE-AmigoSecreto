a7211d1075d7e07183c8c47d859d47e5fa1347ae
Nico <nrgaleano00@gmail.com>
2025-07-23 20:24:44 -0300

    App Update

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let mostrarLista = document.getElementById('listaAmigos');
+let mostrarResultado = document.getElementById('resultado');
+let listaDeNombres = [];
+let textoNombre;
+
+
+function agregarAmigo() {
+    textoNombre = document.getElementById('amigo');
+    mostrarResultado.innerHTML = '';
+    if (textoNombre.value.trim() !== '') {
+        listaDeNombres.push(textoNombre.value.trim());
+        textoNombre.value = '';
+        mostrarLista.innerHTML = 'Nombres: ' + listaDeNombres;
+    } else {
+        alert("El campo esta vacio");
+    }
+}
+
+function sortearAmigo() {
+    if (listaDeNombres.length === 0) {
+        alert("La lista esta vacia");
+        return;
+    }
+    let indiceAleatorio = Math.floor(Math.random() * listaDeNombres.length);
+    let amigoElegido = listaDeNombres[indiceAleatorio];
+
+    mostrarLista.innerHTML = ' ';
+    mostrarResultado.innerHTML = 'El amigo sorteado es: ' + amigoElegido;
+    listaDeNombres = [];
+}
